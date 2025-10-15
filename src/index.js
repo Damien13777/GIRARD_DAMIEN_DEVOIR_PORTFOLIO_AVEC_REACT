96a0b715f0360e234048046bd73c167abcf96e53
Damiengcltsf <dgirard137@gmail.com>
2025-10-15 13:50:05 +0200

    gestion de l'arborescence, ajout des images, creation des pages, re direction des css, ajout de la police et creation de deux variables pour les font weight

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "../src/styles/index.css";
 import App from "./App";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
