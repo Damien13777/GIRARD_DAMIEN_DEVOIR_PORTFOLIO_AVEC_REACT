023dc15d58cc7cceb0f19324a4f4af0b667f3934
Damiengcltsf <dgirard137@gmail.com>
2025-10-15 22:20:43 +0200

    mise en place du routage des pages et creation base des pages pour test d'affichage, concluant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../src/styles/App.css";
+
+//import des pages
+import Contact from "./pages/contact";
+import Home from "./pages/home";
+import Legal from "./pages/legal";
+import Portfolio from "./pages/portfolio";
+import Services from "./pages/services";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App d-flex flex-column min-vh-100">
+        <h1> Hello World</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/legal" element={<Legal />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/services.jsx
@@ -0,0 +1,12 @@
+// pages/home.js - CORRECT
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Page Services</h1>
+    </div>
+  );
+};
+
+export default Services;
