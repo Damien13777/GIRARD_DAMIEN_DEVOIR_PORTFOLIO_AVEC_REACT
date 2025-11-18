0f954800a57b9505d3259b4b32b2ff9635ab2c81
Damiengcltsf <dgirard137@gmail.com>
2025-11-18 21:14:54 +0100

    dev de la page des services et du component service card avec son style,  prochain etape test et controle de conformité avec la maquette

---
+++ src/components/servicecard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import "./servicecard.css";
+
+export default function ServiceCard({ icon, title, description }) {
+  return (
+    <Card className="service-card h-100">
+      <Card.Body className="d-flex flex-column align-items-center text-center p-4">
+        {/* Icône */}
+        <div className="service-icon mb-3">
+          <i className={`bi ${icon}`}></i>
+        </div>
+
+        {/* Titre */}
+        <Card.Title className="service-title mb-3">{title}</Card.Title>
+
+        {/* Description */}
+        <Card.Text className="service-description text-muted">
+          {description}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
